df0299437bb577cefb5d0f1a19379ab02bc1d7f4
Trần Thiện Thanh <kurotsmile@gmail.com>
2024-11-18 00:31:42 +0700

    Update login services

--- app.js
+++ app.js
@@ -53,6 +53,10 @@ app.get('/edit_jobs', (req, res) => {
     res.render('p23_edit_jobs');
 });
 
+app.get('/edit_jobs_interlocks', (req, res) => {
+    res.render('p25_edit_jobs_interlocks');
+});
+
 app.get('/add_pset', (req, res) => {
     res.render('p24_add_pset');
 });
