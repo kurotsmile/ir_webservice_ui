5a92971497cf88ee1e3d18a6d8c63fbe1448c4bb
Trần Thiện Thanh <kurotsmile@gmail.com>
2024-11-18 19:43:38 +0700

    Update all layeout menutop

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.set('views', './src/views');
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
 app.use((req, res, next) => {
+    res.locals.ver = '1.1.0';
     res.locals.currentRoute = req.path;
     next();
 });
