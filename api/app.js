0e69d41f988c65067b71515f4d509aa97a2deb91
Trần Thiện Thanh <kurotsmile@gmail.com>
2024-11-19 08:58:01 +0700

    Add menu  expand

--- api/app.js
+++ api/app.js
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 // Thiết lập thư mục public
 app.use('/public', express.static(path.join(__dirname, '../public')));
 app.use((req, res, next) => {
-    res.locals.ver = '1.1.5';
+    res.locals.ver = '1.1.6';
     res.locals.currentRoute = req.path;
     next();
 });
--- app2.js
+++ app2.js
@@ -8,7 +8,7 @@ app.set('views', './src/views');
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
 app.use((req, res, next) => {
-    res.locals.ver = '1.1.5';
+    res.locals.ver = '1.1.6';
     res.locals.currentRoute = req.path;
     next();
 });
