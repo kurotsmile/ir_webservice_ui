88708a07f24879cde92514632d563f6650ce5833
Trần Thiện Thanh <kurotsmile@gmail.com>
2024-11-20 08:16:53 +0700

    update ui

--- api/app.js
+++ api/app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 
 app.use('/public', express.static(path.join(__dirname, '../public')));
 app.use((req, res, next) => {
-    res.locals.ver = '1.1.8';
+    res.locals.ver = '1.1.9';
     res.locals.currentRoute = req.path;
     next();
 });
