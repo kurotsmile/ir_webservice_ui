154ae9d1cbf4572429deda29cf07cedaad73bad1
Trần Thiện Thanh <kurotsmile@gmail.com>
2024-12-22 18:04:54 +0700

    update fieldbus setting

--- app.js
+++ app.js
@@ -557,6 +557,21 @@ app.get('/fieldbus_io_setting', (req, res) => {
     });
 });
 
+app.get('/fieldbus_diagnostics', (req, res) => {
+    const primaryPath = path.join(get_file_system("Communication"));
+    const secondaryPath = path.join(__dirname, 'public', 'jsonData', 'Communication.json');
+
+    fs.access(primaryPath, fs.constants.F_OK, async (err) => {
+        const filePath = err ? secondaryPath : primaryPath;
+        try {
+            const jsonData = await readJsonFile(filePath);
+            res.render('p42_fieldbus_diagnostics', {jsonData});
+        } catch (error) {
+            res.status(500).send(error.message);
+        }
+    });
+});
+
 app.get('/cycle_result', (req, res) => {
     res.render('p47_cycle_result');
 });
